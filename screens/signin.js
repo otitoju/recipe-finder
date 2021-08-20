46d6da974b2bd09143609248cebb381d4a9adbf6
otitoju <otitojuoluwapelumi@gmail.com>
2021-08-20 01:49:05 -0700

    initial commit

---
+++ screens/signin.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function signin() {
+    return (
+        <View>  
+                
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        paddingRight: 15,
+        paddingLeft: 15,
+        marginRight: 'auto',
+        marginLeft: 'auto'
+    },
+
+    formControl: {
+        display: 'flex',
+        width: '100%',
+        padding:0.375,
+        fontSize: 1,
+        lineHeight: 1.5,
+        color: '#495057',
+        backgroundColor: '#fff',
+        borderRadius: 0.25
+    },
+
+    btn: {
+        display: 'none',
+        textAlign: 'center',
+        padding: 0.375,
+        fontSize: 1,
+        lineHeight: 1.5,
+        borderRadius: 0.25,
+      }
+  });
